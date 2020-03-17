ca0486329f58a96d02e543d14a8f11b425a13aa5
ljg-gui <774609187@qq.com>
2020-03-17 10:53:47 +0800

    上传

---
+++ src/main/java/ljg/asd.java
@@ -0,0 +1,8 @@
+package ljg;
+
+public class asd {
+	public static void main(String[] args) {
+		System.out.println("hello!!");
+		System.out.println("world!!");
+	}
+}
---
+++ src/main/java/ljg/pojo/Users.java
@@ -0,0 +1,15 @@
+package ljg.pojo;
+
+import java.util.Date;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+public class Users {
+	
+	private Integer id;
+
+}
